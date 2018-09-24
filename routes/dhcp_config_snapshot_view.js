39ae7f8f910d9de591197ddbff3eb4ac727e9822
AlexW85 <32299604+AlexW85@users.noreply.github.com>
2018-09-24 12:12:36 +0200

    Update dhcp_config_snapshot_view.js

--- routes/dhcp_config_snapshot_view.js
+++ routes/dhcp_config_snapshot_view.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
+var fs = require('file-system');
 var template_render = require('../core/render-template.js');
 var authorize = require('../core/authorize.js');
 
 router.post('/', authorize.auth, function(req, res, next) {
 	var request = req.body;
-	res.send(fs.readFileSync("./config_backups/" + request.snapshot, 'utf8'));
+	res.send(file-system.readFileSync("./config_backups/" + request.snapshot, 'utf8'));
 });
 
 module.exports = router;
